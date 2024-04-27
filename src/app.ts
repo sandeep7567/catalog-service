4afad5e1bc1def4587808baf4691f62086b62d03
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-27 09:32:58 +0530

    add authenticate middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import config from "config";
+import cookieParser from "cookie-parser";
 
 import categoryRouter from "./category/category-router";
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ ok: config.get("server.port") });
