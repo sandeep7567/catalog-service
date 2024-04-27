86a6556c49739819f62e5f23c04d48a17ae66a45
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-27 09:14:49 +0530

    add asyncHandler

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -3,12 +3,13 @@ import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import { CategoryService } from "./category-service";
 import logger from "../config/logger";
+import { asyncHandler } from "../common/middlewares/utils/asyncHandler";
 
 const router = express.Router();
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
 
-router.post("/", categoryValidator, categoryController.create);
+router.post("/", categoryValidator, asyncHandler(categoryController.create));
 
 export default router;
